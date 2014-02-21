c1e746b0a5f27854b1c8ed437cd0d73e1faaa874
Jouni Kantola <jouni@kantola.se>
2014-02-21 18:33:12 +0100

    Corrected test spec path.

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,22 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
 var paths = {
-  scripts: ['web/scripts/**/*.js'],
-  tests: 'web/tests/**/*.js'
+  scripts: ['web/scripts/**/*.js', '!web/scripts/vendor/*'],
+  tests: 'web/tests/scripts/specs/*.js'
 };
 
 gulp.task('scripts', function() {
   // Minify and copy all JavaScript (except vendor scripts)
   return gulp.src(paths.scripts)
     .pipe(uglify())
-    .pipe(concat('all.min.js'))
+    .pipe(concat('nargarnastatag.min.js'))
     .pipe(gulp.dest('web/build/js'));
 });
 
 // Rerun the task when a file changes
 gulp.task('watch', function () {
   gulp.watch(paths.scripts, ['scripts']);
-  gulp.watch(paths.images, ['images']);
+  gulp.watch(paths.tests, ['tests']);
 });
 
 // The default task (called when you run `gulp` from cli)
